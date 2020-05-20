3fa2ac5039be77d2b632b9504111ce9917e82d9c
Gui Seek <guiseek@gmail.com>
2020-05-19 21:13:57 -0300

    Revert "Add angular material"
    This reverts commit c98beb089a3e9ed9b7e7ed77207cb6d6e6dc48d1.

--- apps/webapp/src/app/app.module.ts
+++ apps/webapp/src/app/app.module.ts
@@ -2,11 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
